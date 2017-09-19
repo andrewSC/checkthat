6dc73bba63c26c827c083c850c374d082cd0c572
Andrew Crerar <ascrerar@ncsu.edu>
2017-09-19 13:35:51 -0400

    Removing ignorelist, finding abs PKGBUILD paths functional

--- checkthat.py
+++ checkthat.py
@@ -5,22 +5,14 @@ import sys
 packages_dir = sys.argv[1]  # ABS path to where all the folders for packages are located
 
 
-def read_ignorelist():
-    with open('ignorelist') as file:
-        return file.read().splitlines()
+def gather_pkgbuild_paths(root_pkgs_dir):
+    pkgbuild_paths = []
+
+    for root, dirs, files in os.walk(root_pkgs_dir):
+        if 'PKGBUILD' in files:
+            pkgbuild_paths.append(os.path.join(root, 'PKGBUILD'))
+
+    print(pkgbuild_paths)
 
 
-def gather_package_paths():
-    dirs_to_ignore = read_ignorelist()  # Read in dirs to ignore/not consider as packages
-
-    for f in os.listdir(packages_dir):
-        actual_abs_path = os.path.abspath('/'.join([packages_dir, f]))
-
-        if os.path.isdir(actual_abs_path) and actual_abs_path not in dirs_to_ignore:
-                yield actual_abs_path
-
-
-for package_path in gather_package_paths():
-    print(f"{package_path}: {os.listdir(package_path)}")
-
-
+gather_pkgbuild_paths(packages_dir)
