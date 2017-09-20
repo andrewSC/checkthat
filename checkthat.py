8c77a6cc298acf09e9acc48c9c70d59d0f3a606c
Andrew Crerar <ascrerar@ncsu.edu>
2017-09-20 10:11:44 -0400

    Stubbing out functions

--- checkthat.py
+++ checkthat.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 
 packages_dir = sys.argv[1]  # ABS path to where all the folders for packages are located
@@ -10,9 +11,29 @@ def gather_pkgbuild_paths(root_pkgs_dir):
 
     for root, dirs, files in os.walk(root_pkgs_dir):
         if 'PKGBUILD' in files:
-            pkgbuild_paths.append(os.path.join(root, 'PKGBUILD'))
+            pkgbuild_paths.append(root)
 
-    print(pkgbuild_paths)
+    return pkgbuild_paths
 
 
-gather_pkgbuild_paths(packages_dir)
+def namcap_check_pkgbuild(pkgbuild_path):
+    abs_pkgbuild_path = '/'.join([pkgbuild_path, 'PKGBUILD'])
+    pkgbuild_path
+    pass
+
+
+def makepkg(pkgbuild_path):
+    pkgbuild_path
+    pass
+
+
+def namcap_check_pkg(pkg_path):
+    pkg_path
+    pass
+
+
+def email_results():
+    pass
+
+
+print(gather_pkgbuild_paths(packages_dir))
