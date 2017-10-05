8dfde1f1bf027a3efbc6139f45032506e23b5d94
Andrew Crerar <andrew@crerar.io>
2017-10-05 09:21:59 -0400

    configuring project to be able to use setuptools

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Checkthat',
+      version='1.0',
+      description='A automated Arch Linux AUR package builder and analyzer written in Python',
+      author='Andrew Crerar',
+      author_email='andrew@crerar.io',
+      url='https://github.com/andrewSC/checkthat',
+      license='MIT',
+      packages=['checkthat'])
