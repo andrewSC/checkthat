8f6ac601047a3073ccc21c0793e30c247c28dcce
Andrew Crerar <andrew@crerar.io>
2017-10-06 11:35:52 -0400

    Read and use config options from config file

--- checkthat/checkthat.py
+++ checkthat/checkthat.py
@@ -4,9 +4,11 @@ import os
 import smtplib
 import toml
 
+from pathlib import Path
+
 from .models import BuildFailure
 from .builders import PackageBuilder
-from .views import EmailView
+from .views import CliView, EmailView
 
 
 def gather_pkgbuild_paths(root_pkgs_dir):
@@ -19,20 +21,13 @@ def gather_pkgbuild_paths(root_pkgs_dir):
     return pkgbuild_paths
 
 
-def email_results(message):
+def email_results(frm, to, subject, message):
     server = smtplib.SMTP('localhost')
     now = datetime.datetime.now()
-    subject = f"Arrakis AUR build results on {now.strftime('%Y-%m-%d %H:%M:%S')}"
-    message = f"From: duncan@planet.arrakis\r\nTo: andrew@crerar.io\r\nSubject: {subject} \r\n\r\n{message}"
+    subject += f" on {now.strftime('%Y-%m-%d %H:%M:%S')}"
+    message = f"From: {frm}\r\nTo: {to}\r\nSubject: {subject} \r\n\r\n{message}"
 
-    email = {
-        'from': 'duncan@planet.arrakis',
-        'to': ['andrew@crerar.io'],
-        'subject': subject,
-        'message': message
-    }
-
-    server.sendmail(email['from'], email['to'], email['message'])
+    server.sendmail(frm, [to], message)
     server.quit()
 
 
@@ -46,24 +41,35 @@ def main():
     parser.add_argument('path', help='Path to where packages are located')
     args = parser.parse_args()
 
+    config_path = '/'.join([str(Path(__file__).resolve().parents[1]), 'config.toml'])
+    config = read_config(config_path)
     abs_paths = gather_pkgbuild_paths(args.path)  # ABS path to where all the folders for packages are located
 
     builder = PackageBuilder()
-    view = EmailView()
     builds = []
 
     for path in abs_paths:
-        build = builder.build(path,
-                              fetch_latest_pkgbuild=True)
-        build.namcap_pkgbuild_analysis = builder.analyze_pkgbuild(path)
+        build = builder.build(path, config['fetch_latest'])
+
+        if config['analyze_pkgbuild']:
+            build.namcap_pkgbuild_analysis = builder.analyze_pkgbuild(path)
 
         if type(build) is not BuildFailure:
             # NOTE: We should only try to analyze the package if the build
             # was actually successful
-            build.namcap_pkg_analysis = builder.analyze_pkg(path)
-            builder.cleanup(build)  # TODO: Make this better
+            if config['analyze_package']:
+                build.namcap_pkg_analysis = builder.analyze_pkg(path)
+
+            if config['cleanup']:
+                builder.cleanup(build)  # TODO: Make this better
 
         builds.append(build)
 
-    # TODO: Make cli output or email results output configurable
-    email_results(view.generate_output(builds))
+    if config['output'] == 'email':
+        email_results(config['email']['addresses']['from'],
+                      config['email']['addresses']['to'],
+                      config['email']['subject'],
+                      EmailView().generate_output(builds))
+
+    elif config['output'] == 'cli':
+        CliView().generate_output(builds)
